aa458f01df89d7414e3074523bbda60c3dc32d2a
Riverhead <james@makerdao.com>
2016-04-28 23:17:16 +0200

    Tweaks and exception handling

--- dashboard.py
+++ dashboard.py
@@ -12,7 +12,7 @@ class Config():
 client = SteemClient(Config)
 
 account = "riverhead"
-miners = ["riverhead","riverhead2","riverhead3","riverhead4","riverhead5","mecon", "jamesreidy"]
+miners = ["riverhead","riverhead2","riverhead3","riverhead4","riverhead5","mecon", "jamesreidy", "riversteem"]
 #os.system('clear')
 print("<HTML><BODY style='background-color:lightgrey;'><table border='1' cellpadding='10'>")
 print("<td valign='top' ><b>Active Witnesses:</b><BR><BR>")
@@ -34,19 +34,18 @@ all_steem = 0
 for miner in miners:
   try:
     miner_info = client.rpc.get_account(miner)
+    chain_info = client.rpc.info()
+    witness_info = client.rpc.get_witness(miner)
+    total_vesting_fund_steem = float(chain_info["total_vesting_fund_steem"][:-5])
+    total_vesting_shares     = float(chain_info["total_vesting_shares"][:-5])
+    balance                  = float(miner_info["vesting_shares"][:-5])
+    total_balance            = round(((total_vesting_fund_steem / total_vesting_shares) * balance), 3)
+    all_steem                = all_steem + total_balance
+    print("<b>%s</b> votes: %s<BR>" % (miner, witness_info["votes"]))
+    print("Last block signed: %s<BR>" % witness_info["last_confirmed_block_num"])
+    print("%s | %s | %s<br><BR>" % (total_balance , miner_info["balance"], miner_info["sbd_balance"]))
   except :
     print("<b>%s</b> has not been mined yet.<BR><BR>" % miner)
-    break
-  chain_info = client.rpc.info()
-  witness_info = client.rpc.get_witness(miner)
-  total_vesting_fund_steem = float(chain_info["total_vesting_fund_steem"][:-5])
-  total_vesting_shares     = float(chain_info["total_vesting_shares"][:-5])
-  balance                  = float(miner_info["vesting_shares"][:-5])
-  total_balance            = round(((total_vesting_fund_steem / total_vesting_shares) * balance), 3)
-  all_steem                = all_steem + total_balance
-  print("<b>%s</b> votes: %s<BR>" % (miner, witness_info["votes"]))
-  print("Last block signed: %s<BR>" % witness_info["last_confirmed_block_num"])
-  print("%s | %s | %s<br><BR>" % (total_balance , miner_info["balance"], miner_info["sbd_balance"]))
 
 print("Head Block: %s <BR>"%chain_info["head_block_number"])
 print("Total STEEM: %s\n" % all_steem)
@@ -116,7 +115,7 @@ for k,v in invotes.items():
     i = i + 1
 
 i=1
-print("</td><td valign='top'><b>No Longer Support You:</b><BR><BR>")
+print("</td><td valign='top'><b>No Longer Supports You:</b><BR><BR>")
 for k,v in invotes.items():
   if v[0] == False:
     print("%s. %s<BR>" % (i,k))
