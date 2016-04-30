c6ac1d34d24617d07fc7292f2d8f9f179fdbd0c0
Riverhead <james@makerdao.com>
2016-04-30 11:29:59 +0200

    Internal documentation

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 while : 
 do 
-  python3 dashboard.py > tmp.html
-  cat tmp.html |grep -v \{\' > index.html 
-  sleep 5
+  python3 dashboard.py > tmp.html # intermediate file to strip off headers from the API
+  cat tmp.html |grep -v \{\' > index.html #This should land in your /var/www/html directory or similar
+  sleep 3 # Refresh interval. Should be at or higher than block period.
 done
